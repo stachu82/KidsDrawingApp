33dc9c4d3eaf7ffdc9c2bf5893b2e2e394905fd2
Stachu82 <stachelek.l@gmail.com>
2020-05-14 23:19:25 +0200

    Drawing App

---
+++ app/src/main/java/eu/learn/kidsdrawingapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package eu.learn.kidsdrawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
